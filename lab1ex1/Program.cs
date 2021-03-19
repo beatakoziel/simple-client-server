a6de60800724536ffbf1c61d79290184f584e4d5
beatinho <beatainez.k@gmail.com>
2021-03-19 11:41:17 +0100

    initial

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace lab1ex1
+{
+    class Program
+    {
+        private static Socket socket;
+        static void Main(string[] args)
+        {
+            Connect();
+            Console.WriteLine("Sending communicate...");
+         
+            string message = null;
+            string textResult = null;
+            while (true)
+            {
+                message = Console.ReadLine();
+                if (message.Equals("quit"))
+                    break;               
+                sendMessage (message);
+                Byte[] receivedBytes = new Byte[100];
+                Byte[] sendBytes = new Byte[100];
+                socket.Receive(receivedBytes, receivedBytes.Length, 0);
+                textResult = System.Text.Encoding.ASCII.GetString(receivedBytes);
+                if (textResult.Length > 0)
+                {
+                    Console.WriteLine(textResult);
+                }
+            }
+        }
+        
+
+        private static void Connect()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress hostAddress = IPAddress.Parse("127.0.0.1");
+            int hostPort = 2222;
+            IPEndPoint hostEndPoint = new IPEndPoint(hostAddress, hostPort);
+            socket.Connect(hostEndPoint);
+        }
+
+        private static void sendMessage(string message)
+        {
+            Byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+            socket.Send(messageBytes, messageBytes.Length, 0);
+        }
+    }
+}
---
+++ lab1ex1_server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace lab1ex1_server
+{
+   
+
+    class Program
+    {
+        private static TcpListener tcpListener;
+        private static Socket socket;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server starting...");
+            Serve();
+        }
+
+        static void Serve()
+        {
+            tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 2222);
+            tcpListener.Start();
+             socket = tcpListener.AcceptSocket();
+            string message = string.Empty;
+            string textResult = string.Empty;
+            while (true)
+            {
+                Byte[] receivedBytes = new Byte[100];
+                Byte[] sendBytes= new Byte[100];
+                socket.Receive(receivedBytes, receivedBytes.Length, 0);
+                textResult = System.Text.Encoding.ASCII.GetString(receivedBytes);
+                if (textResult.Length > 0)
+                {
+                    Console.WriteLine(textResult);
+                }
+                message = Console.ReadLine();
+                if (message.Equals("quit"))
+                    break;
+                sendBytes = System.Text.Encoding.ASCII.GetBytes(message);
+                socket.Send(sendBytes, sendBytes.Length, 0);
+            }
+            tcpListener.Stop();
+        }
+
+        private static void sendMessage(string message)
+        {
+            Byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+            socket.Send(messageBytes, messageBytes.Length, 0);
+        }
+    }
+}
